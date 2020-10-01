599c45dc2a6ed6f2a3500eb85235ce32fbcd2bfa
Kirill Bobyleu <kirill_bobylev19@mail.ru>
2020-10-01 16:17:37 +0300

    feat: test well done

--- src/index.js
+++ src/index.js
@@ -3,23 +3,26 @@ exports.min = function min (array) {
   if (!array || array.length == 0) {
     return 0;
   }
-  return Math.min(array);
+  let otvMin = Math.min(array)
+  return otvMin;
 }
 
 exports.max = function max (array) {
   if (!array || array.length == 0) {
     return 0;
   }
-  return Math.max(array);
+  let otvMax = Math.max(array)
+  return otvMax;
 }
 
 exports.avg = function avg (array) {
-  if (!array || array.length == 0) {
+  if (!array || array.length() == 0) {
     return 0;
   }
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-  return sum / array.length;
+  let otv = sum / array.length();
+  return otv;
 }
