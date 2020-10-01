cd500fd339c40eb2c7a74573c4d17a7d2ee7c1b6
Kirill Bobyleu <kirill_bobylev19@mail.ru>
2020-10-01 17:02:43 +0300

    fix: fixed the code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
 
 exports.min = function min (array) {
-  if (!array || array.length == 0) {
+  if (array === undefined) {
     return 0;
+  } else if (array[0] === undefined) {
+    return 0;
+  } else {
+    let minNumb = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (minNumb > array[i]) {
+        minNumb = array[i];
+      }
+    }
   }
-  let otvMin = Math.min(array)
-  return otvMin;
+  if (array.length === 0){
+    return 0;
+  }else 
+  {
+    return minNumb;
+  }
 }
 
 exports.max = function max (array) {
-  if (!array || array.length == 0) {
-    return 0;
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i];
+    }
   }
-  let otvMax = Math.max(array)
-  return otvMax;
+  return max;
 }
 
 exports.avg = function avg (array) {
-  if (!array || array.length() == 0) {
-    return 0;
-  }
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-  let otv = sum / array.length();
+  let otv = sum / (array.length);
   return otv;
 }
